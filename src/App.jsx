5ac7a00131cda6b3ed7554afda57fbbda4398e26
Malikkkz19 <mnizamidinov@yandex.ru>
2024-03-02 10:17:56 +0300

    create background

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Layout from "./components/Layout/Layout";
+
 function App() {
-  return <>Hello!</>;
+  return <Layout></Layout>;
 }
 
 export default App;
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "./Layout.module.scss";
+
+const Layout = (children) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.images}>
+        <div className={styles.image1}>
+          <img
+            className={styles.image11}
+            src="../src/assets/img/Петр1.png"
+            alt="imperator"
+          />
+        </div>
+        <div className={styles.image2}>
+          <img src="../src/assets/img/имп2.png" alt="imperator" />
+        </div>
+        <div className={styles.image3}>
+          <img src="../src/assets/img/имп3.png" alt="imperator" />
+        </div>
+        <div className={styles.image4}>
+          <img src="../src/assets/img/имп4.png" alt="imperator" />
+        </div>
+        <div className={styles.image5}>
+          <img src="../src/assets/img/имп5.png" alt="imperator" />
+        </div>
+        <div className={styles.image6}>
+          <img src="../src/assets/img/Император_ImgID1.png" alt="imperator" />
+        </div>
+        <div className={styles.image7}>
+          <img src="../src/assets/img/Император_ImgID2.png" alt="imperator" />
+        </div>
+        <div className={styles.image8}>
+          <img src="../src/assets/img/Император_ImgID3.png" alt="imperator" />
+        </div>
+        <div className={styles.image9}>
+          <img src="../src/assets/img/Император_ImgID4.png" alt="imperator" />
+        </div>
+        <div className={styles.image10}>
+          <img src="../src/assets/img/Император_ImgID5.png" alt="imperator" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
