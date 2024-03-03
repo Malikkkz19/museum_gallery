d0e1757a6ef9aa349f7a4499a567647275d8c748
Malikkkz19 <mnizamidinov@yandex.ru>
2024-03-03 14:32:31 +0300

    petr1 to finish

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -8,13 +8,41 @@ const Content = () => {
         <div className={styles.imp1}>
           <div className={styles.description}>
             <img
+              className={styles.descImage}
               height={300}
               width={200}
-              src="/src/assets/img/раммка.png"
+              src="/src/assets/img/museum2.png"
               alt="desc"
             />
           </div>
-          <div className={styles.history}></div>
+          <div className={styles.history}>
+            <img
+              className={styles.imgDecree}
+              height={150}
+              width={180}
+              src="/src/assets/img/УказПетра1.png"
+              alt="decree"
+            />
+            <img
+              height={150}
+              width={126}
+              className={styles.imgBruce}
+              src="/src/assets/img/Брюс.png"
+              alt="Bruce"
+            />
+            <img
+              className={styles.imgGun}
+              src="/src/assets/img/Пушечно-линейныйДвор.png"
+              alt="Пушечно-линейный двор"
+            />
+            <img
+              className={styles.imgTown}
+              height={400}
+              width={400}
+              src="/src/assets/img/СухареваБашня.png"
+              alt="Сухарева Башня на карте"
+            />
+          </div>
         </div>
         <div className={styles.imp2}></div>
         <div className={styles.imp3}></div>
