64bb7e9f92c1e7904470d8541b720df617e7eb8a
Malikkkz19 <mnizamidinov@yandex.ru>
2024-03-23 17:59:31 +0300

    scroll desc

---
+++ src/components/DescImage/DescImage.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+// import { styles } from "./DescImage.module.scss";
+
+// const DescImage = ({ content }) => {
+//   return (
+//     <div className={styles.wrapper}>
+//       <div className={styles.description}>{content}</div>
+//     </div>
+//   );
+// };
+
+// export default DescImage;
