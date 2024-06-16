3394b108f67b0e8bbcae6fa5fd7a21af21af9a32
Malikkkz19 <mnizamidinov@yandex.ru>
2024-06-16 19:30:56 +0300

    background dark

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,33 @@ import styles from "./Header.module.scss";
 const Header = () => {
   return (
     <div className={styles.header}>
-      <div className=""></div>
+      <div className={styles.headerTop}>
+        <div className={styles.Sveyskiy}>
+          <img
+            className={styles.imgSveyskiy}
+            src="/public/Sveyskiy.png"
+            alt="Sveyskiy"
+          />
+        </div>
+        <div className={styles.descSveyskiy}>
+          Командовал
+          <br /> с 1712 года
+          <br /> по 1724 год
+        </div>
+        <div className={styles.Mordvinov}>
+          <img
+            className={styles.imgMordvinov}
+            src="/public/Mordvinov1.png"
+            alt=""
+          />
+        </div>
+        <div className={styles.descMordvinov}>
+          Командовал
+          <br /> в 1749 году
+        </div>
+        <div className={styles.bottomLine}></div>
+      </div>
+      <div className={styles.headerBottom}></div>
     </div>
   );
 };
